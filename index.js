8fb25b65f1da83c397d508fa39975383b97c1b05
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-10-09 09:31:29 -0700

    delete order

--- index.js
+++ index.js
@@ -71,6 +71,14 @@ async function run() {
             res.json(result)
         })
 
+        //DELETE ORDER DATA
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
+
         //USER INFO POST TO THE DATABASE
         app.post('/users', async (req, res) => {
             const user = req.body;
